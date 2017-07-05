72a562b4aaffb11b612c6ded473e90769e0955b4
dbdu <1797890817@qq.com>
2017-07-05 17:07:51 +0800

    fix bug

--- DriverExamSystem/src/main/java/com/java4/des/controller/UserController.java
+++ DriverExamSystem/src/main/java/com/java4/des/controller/UserController.java
@@ -15,28 +15,28 @@ import com.java4.des.service.UserService;
 
 @Controller
 public class UserController {
-	
+
 	private static Logger log = Logger.getLogger(UserController.class);
 
 	@Autowired
 	private UserService userService;
-	
-	@RequestMapping(value="/index")
-	public String index(){
+
+	@RequestMapping(value = "/index")
+	public String index() {
 		System.out.println("index method run....");
 		return "index";
 	}
-	
+
 	@ResponseBody
-	@RequestMapping(value="/json")
+	@RequestMapping(value = "/json")
 	public List<String> json() {
 		List<String> list = new ArrayList<String>();
-		for (int i = 0; i<10; i++){
+		for (int i = 0; i < 10; i++) {
 			list.add("xxxxxx" + i);
 		}
 		return list;
 	}
-	
+
 	@ResponseBody
 	@RequestMapping("/save")
 	public User saveUser() {
@@ -47,23 +47,29 @@ public class UserController {
 		this.userService.saveUser(user);
 		return user;
 	}
+
 	@ResponseBody
 	@RequestMapping("/findbyid/{id}")
-	public User findById(@PathVariable long id){
+	public User findById(@PathVariable long id) {
 		return this.userService.findUserById(id);
 	}
-	
+
 	@ResponseBody
 	@RequestMapping("/findbyusername/{username}")
-	public List<User> findByUsername(@PathVariable String username){
+	public List<User> findByUsername(@PathVariable String username) {
 		System.out.println("In findByUsername");
-		return this.userService.findUserByUsername(username);
+
+		List<User> list = this.userService.findUserByUsername(username);
+		return list;
+
 	}
+
 	
-	@ResponseBody
 	@RequestMapping("/findbypassword/{password}")
-	public List<User> findByPassword(@PathVariable String password){
+	@ResponseBody 
+	public List<User> findByPassword(@PathVariable String password) {
 		System.out.println("In findByPassword!");
-		return this.userService.findUserByPassword(password);
+		List<User> list = this.userService.findUserByPassword(password);
+		return list;
 	}
 }
